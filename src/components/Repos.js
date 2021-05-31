0a191134038f2ce2d4add2317be1d0393ff94c04
Saalu <saaluissak@gmail.com>
2021-05-31 18:46:33 +0000

    Chart info added, showing highest first

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,7 +3,49 @@ import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const {repos} = React.useContext(GithubContext);
+
+  let languages = repos.reduce((total,item) => {
+    const {language} = item;
+    if(!language) return total;
+    if(!total[language]){
+      total[language] = {label:language, value:1}
+    }else{
+      total[language] = {...total[language], 
+        value:total[language].value +1
+      }
+    }
+    return total;
+  },{})
+
+  languages = Object.values(languages).sort((a,b) => {
+    return b.value - a.value;
+  }).slice(0,5)
+
+  console.log(languages);
+
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13"
+    },
+    {
+      label: "CSS",
+      value: "35"
+    },
+    {
+      label: "JavaScript",
+      value: "18"
+    },
+    
+    
+  ];
+  return <section className="section">
+            <Wrapper className='section-center'>
+              <Pie3D data={languages}/>;
+            </Wrapper>
+          </section>
+  
 };
 
 const Wrapper = styled.div`
